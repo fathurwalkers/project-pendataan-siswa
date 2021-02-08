8d3c8273d91e2bac0e3fc522361d00e59881fbe7
Fathur Walkers <fathurwalkers44@gmail.com>
2021-02-08 07:44:44 -0800

    dkwokaoksoq

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Login;
+use App\Detail;
+use App\Absensi;
+use App\Kelas;
+use App\Matapelajaran;
+use App\Nilai;
+use App\Semester;
+use App\Pengajar;
 use Illuminate\Support\Str;
 
 class AdminController extends Controller
