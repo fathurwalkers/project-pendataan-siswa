eed6ca8e7eac1edab5bb0a19197ff53389836a74
Fathur Walkers <fathurwalkers44@gmail.com>
2021-02-13 21:52:22 -0800

    updated

--- database/migrations/2021_02_14_055050_create_semesters_table.php
+++ database/migrations/2021_02_14_055050_create_semesters_table.php
@@ -6,11 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateSemestersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('semester', function (Blueprint $table) {
@@ -29,11 +24,6 @@ class CreateSemestersTable extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('semester');
