6a82eb30e724624739a0f24bba1363376c5083a4
Fathur Walkers <fathurwalkers44@gmail.com>
2021-02-15 05:21:57 -0800

    updated

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,28 +5,28 @@
 @section('header-text', 'Halaman Beranda')
 
 @section('main-content')
-<!-- Default box -->
-<div class="card">
-  <div class="card-header">
-    <h3 class="card-title">Title</h3>
+<div class="col-md-12">
+    <div class="card">
+      <div class="card-body">
 
-    <div class="card-tools">
-      <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-        <i class="fas fa-minus"></i>
-      </button>
-      <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-        <i class="fas fa-times"></i>
-      </button>
+        <div class="col-lg-3 col-6">
+  <!-- small card -->
+          <div class="small-box bg-warning">
+            <div class="inner">
+              <h3>44</h3>
+
+              <p>User Registrations</p>
+            </div>
+            <div class="icon">
+              <i class="fas fa-user-plus"></i>
+            </div>
+            <a href="#" class="small-box-footer">
+              More info <i class="fas fa-arrow-circle-right"></i>
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-  <div class="card-body">
-    Body Section
-  </div>
-  <!-- /.card-body -->
-  <div class="card-footer">
-    Footer Section
-  </div>
-  <!-- /.card-footer-->
 </div>
-<!-- /.card -->
+
 @endsection
