bd2d672912e1d3aa6f2331eddb3b035803c29610
Fathur Walkers <fathurwalkers44@gmail.com>
2021-02-15 05:31:30 -0800

    updated

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -8,22 +8,76 @@
 <div class="col-md-12">
     <div class="card">
       <div class="card-body">
+        <div class="row">
+          <div class="col-md-12 col-lg-3">
 
-        <div class="col-lg-3 col-6">
-  <!-- small card -->
-          <div class="small-box bg-warning">
-            <div class="inner">
-              <h3>44</h3>
+            <div class="small-box bg-warning mx-2">
+              <div class="inner">
+                <h3>44</h3>
+  
+                <p>Total Siswa</p>
+              </div>
+              <div class="icon">
+                <i class="fas fa-user-plus"></i>
+              </div>
+              <a href="#" class="small-box-footer">
+                More info <i class="fas fa-arrow-circle-right"></i>
+              </a>
+            </div>
 
-              <p>User Registrations</p>
-            </div>
-            <div class="icon">
-              <i class="fas fa-user-plus"></i>
-            </div>
-            <a href="#" class="small-box-footer">
-              More info <i class="fas fa-arrow-circle-right"></i>
-            </a>
           </div>
+
+          <div class="col-md-12 col-lg-3">
+            <div class="small-box bg-warning mx-2">
+              <div class="inner">
+                <h3>44</h3>
+  
+                <p>Total Siswa</p>
+              </div>
+              <div class="icon">
+                <i class="fas fa-user-plus"></i>
+              </div>
+              <a href="#" class="small-box-footer">
+                More info <i class="fas fa-arrow-circle-right"></i>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-md-12 col-lg-3">
+            <div class="small-box bg-warning mx-2">
+              <div class="inner">
+                <h3>44</h3>
+  
+                <p>Total Siswa</p>
+              </div>
+              <div class="icon">
+                <i class="fas fa-user-plus"></i>
+              </div>
+              <a href="#" class="small-box-footer">
+                More info <i class="fas fa-arrow-circle-right"></i>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-md-12 col-lg-3">
+            <div class="small-box bg-warning mx-2">
+              <div class="inner">
+                <h3>44</h3>
+  
+                <p>Total Siswa</p>
+              </div>
+              <div class="icon">
+                <i class="fas fa-user-plus"></i>
+              </div>
+              <a href="#" class="small-box-footer">
+                More info <i class="fas fa-arrow-circle-right"></i>
+              </a>
+            </div>
+          </div>
+  
+        </div>
+        
+
         </div>
       </div>
     </div>
