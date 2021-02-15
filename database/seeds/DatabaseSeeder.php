94940c0370a44a08f44356ae0dbc802b3f087476
Fathur Walkers <fathurwalkers44@gmail.com>
2021-02-15 03:17:36 -0800

    updated

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,16 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $faker = Faker::create('id_ID');
-        // for ($i = 0; $i<10; $i++) {
-        //     // $matapelajaran = new Matapelajaran;
-        //     Matapelajaran::create([
-        //         'nama_matapelajaran' => strtoupper($faker->word),
-        //         'kode_matapelajaran' => 'MAPEL-'.strtoupper(Str::random(5)),
-        //         'created_at' => now(),
-        //         'updated_at' => now()
-        //     ]);
-        // }
-        
         $array_mapel = [
             'Matematika',
             'Bahasa Indonesia',
@@ -37,7 +27,6 @@ class DatabaseSeeder extends Seeder
             'Penjaskes',
             'Biologi'
         ];
-
         foreach ($array_mapel as $item) {
             Matapelajaran::create([
                 'nama_matapelajaran' => strtoupper($item),
@@ -46,7 +35,6 @@ class DatabaseSeeder extends Seeder
                 'updated_at' => now()
             ]);
         }
-
         $hashPassword = Hash::make('jancok', [
             'rounds' => 12,
         ]);
@@ -61,108 +49,92 @@ class DatabaseSeeder extends Seeder
             'created_at' => now(),
             'updated_at' => now()
         ]);
-
         $kodekelas = 'KLS-';
         $kodekelas .= strtoupper(Str::random(5));
-
         Kelas::create([
             'kode_kelas' => $kodekelas,
             'kelas' => 'VII A',
             'created_at' => now(),
             'updated_at' => now()
         ]);
-
         Kelas::create([
             'kode_kelas' => $kodekelas,
             'kelas' => 'VII B',
             'created_at' => now(),
             'updated_at' => now()
         ]);
-
         Kelas::create([
             'kode_kelas' => $kodekelas,
             'kelas' => 'VII C',
             'created_at' => now(),
             'updated_at' => now()
         ]);
-
         Kelas::create([
             'kode_kelas' => $kodekelas,
             'kelas' => 'VII D',
             'created_at' => now(),
             'updated_at' => now()
         ]);
-
         Kelas::create([
             'kode_kelas' => $kodekelas,
             'kelas' => 'VII E',
             'created_at' => now(),
             'updated_at' => now()
         ]);
-
         Kelas::create([
             'kode_kelas' => $kodekelas,
             'kelas' => 'VIII A',
             'created_at' => now(),
             'updated_at' => now()
         ]);
-
         Kelas::create([
             'kode_kelas' => $kodekelas,
             'kelas' => 'VIII B',
             'created_at' => now(),
             'updated_at' => now()
         ]);
-
         Kelas::create([
             'kode_kelas' => $kodekelas,
             'kelas' => 'VIII C',
             'created_at' => now(),
             'updated_at' => now()
         ]);
-
         Kelas::create([
             'kode_kelas' => $kodekelas,
             'kelas' => 'VIII D',
             'created_at' => now(),
             'updated_at' => now()
         ]);
-
         Kelas::create([
             'kode_kelas' => $kodekelas,
             'kelas' => 'VIII E',
             'created_at' => now(),
             'updated_at' => now()
         ]);
-
         Kelas::create([
             'kode_kelas' => $kodekelas,
             'kelas' => 'IX A',
             'created_at' => now(),
             'updated_at' => now()
         ]);
-
         Kelas::create([
             'kode_kelas' => $kodekelas,
             'kelas' => 'IX B',
             'created_at' => now(),
             'updated_at' => now()
         ]);
-
         Kelas::create([
             'kode_kelas' => $kodekelas,
             'kelas' => 'IX C',
             'created_at' => now(),
             'updated_at' => now()
         ]);
-
         Kelas::create([
             'kode_kelas' => $kodekelas,
             'kelas' => 'IX D',
             'created_at' => now(),
             'updated_at' => now()
         ]);
-
         Kelas::create([
             'kode_kelas' => $kodekelas,
             'kelas' => 'IX E',
