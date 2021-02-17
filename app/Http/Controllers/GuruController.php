fc1a9a81c568a768148e68d1b899eb63ee2dd02d
Fathur Walkers <fathurwalkers44@gmail.com>
2021-02-17 01:24:22 -0800

    update

--- app/Http/Controllers/GuruController.php
+++ app/Http/Controllers/GuruController.php
@@ -3,6 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Login;
+use App\Detail;
+use App\Absensi;
+use App\Kelas;
+use App\Matapelajaran;
+use App\Nilai;
+use App\Semester;
+use App\Pengajar;
+use Illuminate\Support\Str;
+use Faker\Factory as Faker;
+use Illuminate\Support\Arr as Randoms;
 
 class GuruController extends Controller
 {
