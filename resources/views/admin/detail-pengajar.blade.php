c6b1f10641ce84220313e5b218097b02c6cecacc
Fathur Walkers <fathurwalkers44@gmail.com>
2021-02-19 22:12:11 -0800

    update

--- resources/views/admin/detail-pengajar.blade.php
+++ resources/views/admin/detail-pengajar.blade.php
@@ -5,13 +5,27 @@
 @section('header-text', 'Detail Pengajar')
 
 @section('main-content')
-<div class="card w-75">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">
-        With supporting text below as a natural lead-in to additional content.
-      </p>
-      <a href="#" class="btn btn-primary">Button</a>
+<div class="row">
+
+    <div class="card col-sm-6 col-lg-6">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">
+            With supporting text below as a natural lead-in to additional content.
+          </p>
+          <a href="#" class="btn btn-primary">Button</a>
+        </div>
     </div>
+
+    <div class="card col-sm-6 col-lg-6">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">
+            With supporting text below as a natural lead-in to additional content.
+          </p>
+          <a href="#" class="btn btn-primary">Button</a>
+        </div>
+    </div>
+
 </div>
 @endsection
