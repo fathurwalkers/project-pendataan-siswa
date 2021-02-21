6413c5d3d0449954a3afbff1edf1217f05d39527
Fathur Walkers <fathurwalkers44@gmail.com>
2021-02-20 21:21:33 -0800

    update

--- database/migrations/2021_02_14_055820_create_nilais_table.php
+++ database/migrations/2021_02_14_055820_create_nilais_table.php
@@ -6,11 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateNilaisTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('nilai', function (Blueprint $table) {
@@ -34,11 +29,6 @@ class CreateNilaisTable extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('nilai');
